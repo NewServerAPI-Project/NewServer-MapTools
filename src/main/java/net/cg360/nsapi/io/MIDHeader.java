fba5ca906992d6f2a193b84ab89248ec9c9c30e4
CG360 <CloudGamer360@users.noreply.github.com>
2021-01-16 17:46:25 +0000

    Added header parsing from json.

--- src/main/java/net/cg360/nsapi/io/MIDHeader.java
+++ src/main/java/net/cg360/nsapi/io/MIDHeader.java
@@ -1,6 +1,10 @@
 package net.cg360.nsapi.io;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import net.cg360.nsapi.commons.Utility;
+import net.cg360.nsapi.commons.exception.MissingPropertyException;
 
 import java.util.Optional;
 
@@ -15,8 +19,7 @@ public final class MIDHeader {
     private String mapStorageType;
     private String mapStorageVersion;
 
-
-    protected MIDHeader(String mapPath, String identifier, Integer formatVersion, String mapStorageType, String mapStorageVersion) {
+    public MIDHeader(String mapPath, String identifier, Integer formatVersion, String mapStorageType, String mapStorageVersion) {
         if(formatVersion == null) throw new IllegalArgumentException("MapID is missing a 'format_version' property.");
 
         this.mapPath = mapPath;
@@ -24,6 +27,39 @@ public final class MIDHeader {
         this.formatVersion = formatVersion;
         this.mapStorageType = mapStorageType == null ? "null" : mapStorageType.trim().toLowerCase();
         this.mapStorageVersion = mapStorageVersion == null ? "null" : mapStorageVersion.trim().toLowerCase();
+    }
+
+    public static MIDHeader getHeaderFromJson(JsonObject root) { return getHeaderFromJson(root, null); }
+    public static MIDHeader getHeaderFromJson(JsonObject root, String mapPath) {
+        Integer formatV = null;
+        String id = null, mapStorage = null, mapStorageV = null;
+
+        JsonElement formatElement = root.get("format_version");
+        if(formatElement instanceof JsonPrimitive){
+            JsonPrimitive p = (JsonPrimitive) formatElement;
+            if (p.isNumber()) formatV = p.getAsNumber().intValue();
+        }
+        if(formatV == null) throw new MissingPropertyException("MapID is missing a 'format_version' property.");
+
+        JsonElement idElement = root.get("identifier");
+        if(idElement instanceof JsonPrimitive){
+            JsonPrimitive p = (JsonPrimitive) idElement;
+            id = p.getAsString();
+        }
+
+        JsonElement mapStoreElement = root.get("map_storage_type");
+        if(mapStoreElement instanceof JsonPrimitive){
+            JsonPrimitive p = (JsonPrimitive) mapStoreElement;
+            mapStorage = p.getAsString();
+        }
+
+        JsonElement mapStoreVElement = root.get("map_storage_version");
+        if(mapStoreVElement instanceof JsonPrimitive){
+            JsonPrimitive p = (JsonPrimitive) mapStoreVElement;
+            mapStorageV = p.getAsString();
+        }
+
+        return new MIDHeader(mapPath, id, formatV, mapStorage, mapStorageV);
     }
 
     public Optional<String> getMapPath() { return Optional.ofNullable(mapPath); }

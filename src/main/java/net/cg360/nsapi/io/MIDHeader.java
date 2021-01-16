5134e018ed1730be878148e41e8603c4ea935548
CG360 <CloudGamer360@users.noreply.github.com>
2021-01-16 15:45:34 +0000

    - Made header more lenient with missing values - MapStorageVersion is now a string - mapPath now belongs in the header.

--- src/main/java/net/cg360/nsapi/io/MIDHeader.java
+++ src/main/java/net/cg360/nsapi/io/MIDHeader.java
@@ -1,31 +1,34 @@
 package net.cg360.nsapi.io;
 
+import net.cg360.nsapi.commons.Utility;
+
+import java.util.Optional;
+
 /**
  * @author CG360;
  */
 public final class MIDHeader {
 
+    private String mapPath;
     private String identifier;
     private Integer formatVersion;
     private String mapStorageType;
-    private Integer mapStorageVersion;
+    private String mapStorageVersion;
 
 
-    protected MIDHeader(String identifier, Integer formatVersion, String mapStorageType, Integer mapStorageVersion) {
-        if(identifier == null) throw new IllegalArgumentException("MapID is missing a map 'identifier' property.");
+    protected MIDHeader(String mapPath, String identifier, Integer formatVersion, String mapStorageType, String mapStorageVersion) {
         if(formatVersion == null) throw new IllegalArgumentException("MapID is missing a 'format_version' property.");
-        if(mapStorageType == null) throw new IllegalArgumentException("MapID is missing a 'map_storage_type' property.");
-        if(mapStorageVersion == null) throw new IllegalArgumentException("MapID is missing a 'map_storage_version' property.");
 
-
-        this.identifier = identifier.trim().toLowerCase();
+        this.mapPath = mapPath;
+        this.identifier = identifier == null ? "generated-"+ Utility.generateUniqueToken(5, 3).toLowerCase() : identifier.trim().toLowerCase();
         this.formatVersion = formatVersion;
-        this.mapStorageType = mapStorageType.trim().toLowerCase();
-        this.mapStorageVersion = mapStorageVersion;
+        this.mapStorageType = mapStorageType == null ? "null" : mapStorageType.trim().toLowerCase();
+        this.mapStorageVersion = mapStorageVersion == null ? "null" : mapStorageVersion.trim().toLowerCase();
     }
 
+    public Optional<String> getMapPath() { return Optional.ofNullable(mapPath); }
     public String getIdentifier() { return identifier; }
     public Integer getFormatVersion() { return formatVersion; }
     public String getMapStorageType() { return mapStorageType; }
-    public Integer getMapStorageVersion() { return mapStorageVersion; }
+    public String getMapStorageVersion() { return mapStorageVersion; }
 }

59a21ac444cb506d9a6a1a6113258c704207a36d
CG360 <CloudGamer360@users.noreply.github.com>
2021-01-16 16:17:52 +0000

    Implement MapID structure

---
+++ src/main/java/net/cg360/nsapi/MapID.java
@@ -0,0 +1,84 @@
+package net.cg360.nsapi;
+
+import net.cg360.nsapi.commons.Utility;
+import net.cg360.nsapi.commons.data.MapRegionDataStore;
+import net.cg360.nsapi.commons.data.PointEntityDataStore;
+import net.cg360.nsapi.commons.math.PosRot;
+import net.cg360.nsapi.io.MIDHeader;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author CG360;
+ */
+public abstract class MapID {
+
+    private MIDHeader header;
+
+    private String displayName;
+    private String description;
+    private String[] authors;
+    private String[] supportedGamemodes;
+
+    private Map<String, PosRot[]> spawns;
+    private Map<String, MapRegionDataStore> regions;
+    private Map<String, PointEntityDataStore> pointEntities;
+    private Map<String, String> strings;
+    private Map<String, Number> numbers;
+    private Map<String, Boolean> switches;
+
+
+    public MapID(MIDHeader header, String displayName, String description, String[] authors, String[] supportedGamemodes, Map<String, PosRot[]> spawns, Map<String, MapRegionDataStore> regions, Map<String, PointEntityDataStore> pointEntities, Map<String, String> strings, Map<String, Number> numbers, Map<String, Boolean> switches) {
+        if(header == null) throw new IllegalArgumentException("MapID is somehow missing a header. This is a plugin bug, please report."); //Whoever triggers this will make me screeeech.
+        if(spawns == null || spawns.size() == 0) throw new IllegalArgumentException("MapID is missing spawns. A map must have at *least* 1 spawn.");
+
+        this.header = header;
+        this.displayName = displayName == null ? Utility.pickRandomString(MapIDConstants.MAPID_MISSING_NAMES) : displayName;
+        this.description = description == null ? Utility.pickRandomString(MapIDConstants.MAPID_MISSING_DESCRIPTIONS) : description;;
+        this.authors = authors == null ? new String[]{"Unknown"} : authors; //Maybe use contributors? Seems like a potentially bad way to deal with it. Opinions?
+        this.supportedGamemodes = supportedGamemodes == null ? new String[0] : supportedGamemodes;
+        this.spawns = spawns;
+        this.regions = regions == null ? Collections.unmodifiableMap(new HashMap<>()) : regions;
+        this.pointEntities = pointEntities == null ? Collections.unmodifiableMap(new HashMap<>()) : pointEntities;
+        this.strings = strings == null ? Collections.unmodifiableMap(new HashMap<>()) : strings;
+        this.numbers = numbers == null ? Collections.unmodifiableMap(new HashMap<>()) : numbers;
+        this.switches = switches == null ? Collections.unmodifiableMap(new HashMap<>()) : switches;
+    }
+
+    public MIDHeader getHeader() { return header; }
+    public String getDisplayName() { return displayName; }
+    public String getDescription() { return description; }
+    public String[] getAuthors() { return authors; }
+    public String[] getSupportedGamemodes() { return supportedGamemodes; }
+    public Map<String, PosRot[]> getSpawns() { return spawns; }
+    public Map<String, MapRegionDataStore> getRegions() { return regions; }
+    public Map<String, PointEntityDataStore> getPointEntities() { return pointEntities; }
+    public Map<String, String> getStrings() { return strings; }
+    public Map<String, Number> getNumbers() { return numbers; }
+    public Map<String, Boolean> getSwitches() { return switches; }
+
+
+    public static class AssembledMapID extends MapID {
+
+    }
+
+
+
+    public static class Builder extends MapID {
+
+        public Builder(String displayName, String[] supportedGamemodes) {
+
+        }
+
+        protected Builder() {
+
+        }
+
+        public MapID build() {
+            return new AssembledMapID();
+        }
+
+    }
+}
--- src/main/java/net/cg360/nsapi/MapIDConstants.java
+++ src/main/java/net/cg360/nsapi/MapIDConstants.java
@@ -8,4 +8,34 @@ public class MapIDConstants {
     public static final int MAPID_FORMAT_VERSION = 2;
     public static final String[] CONTRIBUTORS = new String[]{ "CG360" };
 
+    public static final String[] MAPID_MISSING_NAMES = new String[] {
+            "Mapville, Mapshire",
+            "ThereIsNoMap",
+            "Canada",
+            "The United Kingdom",
+            "Where am I?",
+            "MapID Project",
+            "Example",
+            "ExampleMap",
+            "Pickle",
+            "Husky",
+            "CG360's Domain",
+            "Vibezone",
+            "Jooce Zone",
+            "Under da sea",
+            "[ERROR]",
+            "*insert big source engine error*"
+    };
+
+    public static final String[] MAPID_MISSING_DESCRIPTIONS = new String[] {
+            "It appears you have managed to find a broken map.",
+            "Beepity boop, something something monkeys fixing the issue.",
+            "QUICK! A DISTRACTION!",
+            "But will it be in smash?",
+            "It appears you've joined a server that cannot setup maps with proper descriptions. Oh well.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, pulvinar facilisis justo mollis, auctor consequat urna. ",
+            "This map got 2020'd lmao.",
+            "Land of corgi's, crumpets, and tea."
+    };
+
 }

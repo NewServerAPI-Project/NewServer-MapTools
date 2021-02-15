47e4a2fb02f2f6062d5e7dea3348c60d3f9602af
Will <willscullythewag@gmail.com>
2021-02-15 19:41:51 +0000

    Created utility class for Cube16

---
+++ nsmtcommon/src/main/java/net/cg360/nsapi/map/world/cube16/Cube16Utility.java
@@ -0,0 +1,30 @@
+package net.cg360.nsapi.map.world.cube16;
+
+import net.cg360.nsapi.map.world.cube16.format.Cube16Chunk;
+
+public class Cube16Utility {
+
+    protected static int getBlockIndex(int x, int y, int z) {
+        return x + (z * Cube16Chunk.CHUNK_SIZE) + (y * Cube16Chunk.CHUNK_SIZE ^ 2);
+    }
+
+    /**
+     * Calculates the byte (and bit) starting position for a block to be written.
+     * @return 2 ints | 1st = the byte in the ByteBuffer, 2nd = the bit within the byte where the block starts.
+     */
+    protected static short[] getBufferIndex(int bitDepth, int x, int y, int z) {
+        int index = getBlockIndex(x, y, z);
+
+        // Scale it down so that the index is divided up.
+        // Before decimal place = byte location.
+        // After decimal place = bit location / 8
+        float scaled = index * (((float) bitDepth) / 8f);
+        float remainder = (scaled % 1);
+
+        short byteIndex = (short) (scaled - remainder); // Could support a 32x format.
+        short subIndex = (short) (remainder * 8); // The most it could be is 8. Keep it as a short for ease of use. Byte would be better.
+
+        return new short[]{byteIndex, subIndex};
+    }
+
+}

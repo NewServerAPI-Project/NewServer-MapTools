18468ba33e3de5c9abdb57668a770c3b1bc521ec
Will <willscullythewag@gmail.com>
2021-02-15 20:46:39 +0000

    Introduce Cube16Chunk + Cube16BlockBuffer

---
+++ nsmtcommon/src/main/java/net/cg360/nsapi/map/world/cube16/Cube16BlockBuffer.java
@@ -0,0 +1,112 @@
+package net.cg360.nsapi.map.world.cube16;
+
+import net.cg360.nsapi.commons.math.NSMath;
+import net.cg360.nsapi.map.io.upgrade.world.Block;
+import net.cg360.nsapi.map.world.cube16.util.Cube16Utility;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+
+/**
+ * A class used to interface with a Cube16Chunk's block buffer without
+ * the
+ */
+public class Cube16BlockBuffer {
+
+    protected Cube16Chunk chunk;
+    protected ByteBuffer blockBuffer;
+
+    protected byte lastBitIndex = 0;
+
+    protected Cube16BlockBuffer(Cube16Chunk chunk, ByteBuffer sourceBuffer) {
+        this.chunk = chunk;
+        sourceBuffer.duplicate();
+    }
+
+    /** Sets the position of the block buffer to 0 */
+    public void resetBlockBufferPosition() {
+        blockBuffer.clear();
+        lastBitIndex =  0;
+    }
+
+    /** Sets the position of the block buffer to the start of the block coords specified. */
+    public void prepareBlockBufferPosition(int x, int y, int z) {
+        blockBuffer.clear(); // Resets position to start. Doesn't actually clear.
+        short[] position = Cube16Utility.getBufferIndex(chunk.getPaletteBitDepth(), x, y, z);
+        blockBuffer.position(position[0]);
+        lastBitIndex = (byte) position[1]; // Its a byte unless something went wrong.
+    }
+
+    /** @return the next block available in the buffer. */
+    public Block getNextBlock() {
+        List<Block> palette = chunk.getPalette();
+        if(palette.size() == 1) { // Size should always be >1
+            return palette.get(0);
+        } else {
+            return palette.get(getNextBlockBufferValue());
+        }
+    }
+
+    /** The palette ID of the next block in the buffer. */
+    public int getNextBlockBufferValue() {
+        int value = 0;
+        int n = 1;
+
+        if(lastBitIndex > 0){
+            // Re-read the byte. Could've been modified?
+            blockBuffer.position(blockBuffer.position() - 1);
+        }
+
+        byte requiredByteCount = (byte) Math.ceil( ((float) chunk.getPaletteBitDepth()) / 8 );
+        byte lastReadIndex = lastBitIndex;
+
+        for(int i = 0; i < requiredByteCount; i++){
+            byte singleByte = blockBuffer.get();
+
+            // Bit position is less than the distance to the next block
+            // Bit position is less than 8 bits.
+            // This works as long as depths are kept to 2^n. Else it starts missing bits.
+            for(byte b = lastReadIndex; (b < (lastReadIndex + chunk.getPaletteBitDepth())) && (b < 8); b++) {
+                value += (NSMath.getBit(singleByte, b) * (2 ^ n));
+                n++;
+                lastBitIndex++; // Update the tracker.
+            }
+
+            if(lastBitIndex >= 8){
+                lastBitIndex = 0; // Reset cached bit index when a new byte is read.
+            }
+        }
+
+        return value;
+    }
+
+    /** @return returns the chunk this buffer accesses. */
+    public Cube16Chunk getChunk() {
+        return chunk;
+    }
+
+    /*
+        I did some testing in python to get the above method right (Arrrrggghhh this took a bit. Smol brain)
+        bitDepth = 2
+
+        def prg(x, y, z):
+            index = x + (z * 16) + (y * 256)
+
+            scaled = index * (bitDepth / 8)
+            remainder = (scaled % 1)
+            subIndex = remainder * 8
+            byteIndex = scaled - remainder
+
+
+            print(byteIndex, ":", subIndex,"|",index)
+            input("")
+
+        prg(15, 15, 15)  # Check that it's correctly lined up
+
+        for y in range(0, 16):
+            for z in range(0, 16):
+                for x in range(0, 16):
+                    prg(x, y, z)
+     */
+
+}
---
+++ nsmtcommon/src/main/java/net/cg360/nsapi/map/world/cube16/Cube16Chunk.java
@@ -0,0 +1,67 @@
+package net.cg360.nsapi.map.world.cube16;
+
+import net.cg360.nsapi.map.io.upgrade.world.Block;
+import net.cg360.nsapi.map.world.cube16.Cube16BlockBuffer;
+import net.cg360.nsapi.map.world.cube16.enums.Cube16Encode;
+import se.llbit.nbt.CompoundTag;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+
+/**
+ * The API-side representation of a Cube16 chunk.
+ * It combines the block chunk and the entity chunk files into
+ * one object to access reasons.
+ *
+ */
+public abstract class Cube16Chunk {
+
+    public static final int CHUNK_SIZE = 16; // This'll make scaling to 32x easier. May need to replace some datatypes.
+    public static final int BIOME_SCALE = 4; // The amount of blocks biome data covers. Vanilla is 4x4x4 as of 1.15
+    public static final byte[] SUPPORTED_PALETTE_SCALES = { 1, 2, 4, 8, 16 };
+
+    // Blocks: Serialization
+    protected Cube16Encode encodeMode;
+    protected List<Block> palette;
+    protected List<CompoundTag> tileEntities;
+    protected ByteBuffer blockData; // length = 16x16x16x(depth/8)
+    protected int paletteBitDepth; // Should recalculate blockData every time this changes.
+
+    protected List<CompoundTag> chunkEntityData; // Only stores the last saved/loaded state the the chunks entities.
+
+    // -- Access --
+    protected Cube16BlockBuffer primaryBlockBuffer;
+    protected boolean isImmutable;
+
+
+    /**
+     * Get a block from the blockData. Less efficient than direct
+     * access as it required the buffer position to be set.
+     * @param x x coordinate of the block.
+     * @param y y coordinate of the block.
+     * @param z z coordinate of the block.
+     * @return the block (ID + Blockstate)
+     */
+    public Block getBlock(int x, int y, int z) {
+        getPrimaryBlockBuffer().prepareBlockBufferPosition(x, y, z);
+        return getPrimaryBlockBuffer().getNextBlock();
+    }
+
+    /** @return The shared main block buffer for accessing blockData.*/
+    public Cube16BlockBuffer getPrimaryBlockBuffer() {
+        if(primaryBlockBuffer == null) primaryBlockBuffer = getBlockBuffer();
+        return primaryBlockBuffer;
+    }
+
+    /** @return A new block buffer for accessing blockData.*/
+    public Cube16BlockBuffer getBlockBuffer() {
+        return new Cube16BlockBuffer(this, blockData);
+    }
+
+    public Cube16Encode getEncodeMode() { return encodeMode; }
+    public List<Block> getPalette() { return palette; }
+    public int getPaletteBitDepth() { return paletteBitDepth; }
+
+    public List<CompoundTag> getTileEntities() { return tileEntities; }
+    public List<CompoundTag> getChunkEntityData() { return chunkEntityData; }
+}
